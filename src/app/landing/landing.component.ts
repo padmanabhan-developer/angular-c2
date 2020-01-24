35e4c395cc96663adf72465e63fa3e1c83751e32
padmanabhan-developer <padmanabhan.developer.email@gmail.com>
2020-01-24 18:26:50 +0530

    6 files modified

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbCarousel, NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap'
+import { NgbCarousel, NgbCarouselConfig, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap'
 @Component({
   selector: 'landing',
   templateUrl: './landing.component.html',
